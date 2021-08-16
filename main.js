3dd1d8ccc5f89d592c5a0c400d5835d14852cc84
Leo <76232822+Leo-Yan-150@users.noreply.github.com>
2021-08-15 18:20:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-noseX = 0;
-noseY = 0;
-difference = 0;
-rwX = 0;
-lwX = 0;
-t = "( ͡° ͜ʖ ͡°)"
 function preload(){
 
 }
@@ -20,29 +14,6 @@ function setup(){
 
 }
 function draw(){
-    background(255);
-    document.getElementById("text_side").innerHTML = "width and height of the text will be = " + difference + " px.";
-
-    stroke(0);
-    textSize(difference);
-    fill(255,0,0);
-    if(key === "q" && keyIsPressed){
-        fill(0,255,0);
-        t = "( ͡° ͜ʖ ͡°)"
-    }
-    if(key === "w" && keyIsPressed){
-        fill(0,0,255);
-        t = ":("
-    }
-    if(key === "e" && keyIsPressed){
-        fill(255,255,0);
-        t = ":0"
-    }
-    if(key === "r" && keyIsPressed){
-        fill(0,255,255);
-        t = ":)"
-    }
-    text(t, noseX, noseY);
 
 }
 function modelLoaded(){
@@ -51,13 +22,5 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
-        console.log("nose X = " + noseX + " nose Y = " + noseY);
-        
-        lwX = results[0].pose.leftWrist.x;
-        rwX = results[0].pose.rightWrist.x;
-        difference = floor(lwX - rwX);
-        console.log("left wrist x = " + lwX + " right wrist x = " + rwX + " difference = " + difference);
     }
 }
